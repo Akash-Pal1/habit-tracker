2ea1337a3f86cff09382817b2e811f2a702dfd65
Akash <apal62214@gmail.com>
2026-06-14 16:50:56 +0530

    added helper function for streak calculations

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from app import models, schemas
 from app.database import engine, get_db
+from datetime import date, timedelta
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -18,6 +19,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def calculate_streak(completions):
+    if not completions:
+        return 0
+    
+    dates = sorted([c.completed_date for c in completions], reverse=True)
+    # dates = sorted(completions, reverse=True) -- useed for checking and testing purpose
+    
+
+    if dates[0] < date.today() - timedelta(days=1):
+        return 0 
+    
+    streak = 1
+    for i in range(1, len(dates)):
+        if(dates[i-1] - dates[i]).days == 1:
+            streak +=1 
+        else:
+            break
+
+    return streak
 
 @app.get("/")
 def root():
